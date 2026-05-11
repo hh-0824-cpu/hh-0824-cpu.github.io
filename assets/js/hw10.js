8735d4099f696306668d1bf7baa29fa53f9ed05e
조하현 <hh-0824@snu.ac.kr>
2026-05-11 01:55:50 +0000

    fix hw10

--- assets/js/hw10.js
+++ assets/js/hw10.js
@@ -96,5 +96,6 @@ for (let i = 1; i < counts.length; i++) {
     }
 }
 
-const top = targets[maxIdx];
-console.log(`가장 자주 나온 글자: '${top}' (${counts[maxIdx]}번)`);
+const topChar = targets[maxIdx];
+
+console.log(`가장 자주 나온 글자: '${topChar}' (${counts[maxIdx]}번)`);
